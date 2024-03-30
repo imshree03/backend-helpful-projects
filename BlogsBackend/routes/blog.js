1d22e357362d6cbf8a518f6fee70f62747063ea7
imshree03 <shreebhosale24hrs@gmail.com>
2024-03-30 18:24:00 +0530

    routes

---
+++ BlogsBackend/routes/blog.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { createComment } = require("../controllers/commentController");
+const { createPost, getAllPosts } = require("../controllers/postController");
+const { likePost, unlikePost } = require("../controllers/likeController");
+const router = express.Router();
+
+router.post("/comments/create", createComment);
+router.post("/posts/create", createPost);
+router.get("/posts/getAllposts", getAllPosts);
+router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
+module.exports = router;
